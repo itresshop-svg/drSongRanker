84d3fc80b869dfdcd3a6940823318cb8110342d0
itresshop-svg <itresshop@gmail.com>
2026-03-16 05:43:26 +0300

    Update app_song_data.js

--- app_song_data.js
+++ app_song_data.js
@@ -1,24 +1,14 @@
 const songList = [
   { id: 1, name: "SA1: Open Your Heart", file: "SA1/01.mp3", rating: 1500, comparisons: 0 },
-  { id: 2, name: "SA1: It Doesn't Matter", file: "SA1/02.mp3", rating: 1500, comparisons: 0 },
-  { id: 3, name: "SA1: Believe in Myself", file: "SA1/03.mp3", rating: 1500, comparisons: 0 },
-  { id: 4, name: "SA1: Unknown from M.E.", file: "SA1/04.mp3", rating: 1500, comparisons: 0 },
-  { id: 5, name: "SA1: Emerald Coast", file: "SA1/08.mp3", rating: 1500, comparisons: 0 },
-  { id: 6, name: "SA1: Windy Valley", file: "SA1/10.mp3", rating: 1500, comparisons: 0 },
-  { id: 7, name: "SA1: Speed Highway", file: "SA1/16.mp3", rating: 1500, comparisons: 0 },
-  { id: 8, name: "SA1: Ice Cap", file: "SA1/14.mp3", rating: 1500, comparisons: 0 },
-  { id: 9, name: "SA2: Live & Learn", file: "SA2/01.mp3", rating: 1500, comparisons: 0 },
-  { id: 10, name: "SA2: City Escape", file: "SA2/02.mp3", rating: 1500, comparisons: 0 },
-  { id: 11, name: "SA2: Throw It All Away", file: "SA2/06.mp3", rating: 1500, comparisons: 0 },
-  { id: 12, name: "SA2: Pumpkin Hill", file: "SA2/09.mp3", rating: 1500, comparisons: 0 },
-  { id: 13, name: "SA2: E.G.G.M.A.N.", file: "SA2/07.mp3", rating: 1500, comparisons: 0 },
-  { id: 14, name: "SA2: Radical Highway", file: "SA2/13.mp3", rating: 1500, comparisons: 0 },
-  { id: 15, name: "SA2: Supporting Me", file: "SA2/22.mp3", rating: 1500, comparisons: 0 },
-  { id: 16, name: "Heroes: Sonic Heroes", file: "SH/01.mp3", rating: 1500, comparisons: 0 },
-  { id: 17, name: "Heroes: We Can", file: "SH/02.mp3", rating: 1500, comparisons: 0 },
-  { id: 18, name: "Heroes: This Machine", file: "SH/03.mp3", rating: 1500, comparisons: 0 },
-  { id: 19, name: "Heroes: Seaside Hill", file: "SH/07.mp3", rating: 1500, comparisons: 0 },
-  { id: 20, name: "Heroes: What I'm Made Of", file: "SH/06.mp3", rating: 1500, comparisons: 0 }
+  { id: 2, name: "SA2: Live & Learn", file: "SA2/01.mp3", rating: 1500, comparisons: 0 },
+  { id: 3, name: "SA2: City Escape", file: "SA2/02.mp3", rating: 1500, comparisons: 0 },
+  { id: 4, name: "Heroes: Sonic Heroes", file: "SH/01.mp3", rating: 1500, comparisons: 0 },
+  { id: 5, name: "UT: Home (Music Box)", file: "UT/home.mp3", rating: 1500, comparisons: 0 },
+  { id: 6, name: "UT: Megalovania", file: "UT/mega.mp3", rating: 1500, comparisons: 0 },
+  { id: 7, name: "UTY: Remedy", file: "UTY/remedy.mp3", rating: 1500, comparisons: 0 },
+  { id: 8, name: "UTY: Retribution", file: "UTY/retribution.mp3", rating: 1500, comparisons: 0 },
+  { id: 9, name: "TS!US: Bonequarrel", file: "TSUS/bone.mp3", rating: 1500, comparisons: 0 },
+  { id: 10, name: "DR: BIG SHOT", file: "DR/bigshot.mp3", rating: 1500, comparisons: 0 }
 ];
 
 export default songList;
